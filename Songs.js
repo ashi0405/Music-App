56f3aeacb576100cb824a7146383cbd233339ffc
ashi0405 <aashitabajaj2@gmail.com>
2021-06-16 23:32:40 +0530

    changes

---
+++ Songs.js
@@ -0,0 +1,64 @@
+let Songs=[
+    {
+        "Title": "Faded",
+        "Artist": "Alan Walker",
+        "URL": "./Songs/alan-walker-faded.mp3"
+      },
+      {
+        "Title": "Closer",
+        "Artist": "The Chainsmokers",
+        "URL": "./Songs/Closer - The Chainsmokers.mp3"
+      },
+      {
+        "Title": "Despacito",
+        "Artist": "Daddy Yankee",
+        "URL": "./Songs/despacito-ft-daddy-yankee.mp3"
+      },
+      {
+        "Title": "Let Me Love You",
+        "Artist": "Justin Bieber",
+        "URL": "./Songs/let-me-love-you.mp3"
+      },
+      {
+        "Title": "Love Me Like You Do",
+        "Artist": "Ellie Goulding",
+        "URL": "./Songs/Love Me Like You Do Full Mp3 Song Download 2021(SongsZilla.Net).mp3"
+      },
+      {
+        "Title": "Girls Like You",
+        "Artist": "Maroon 5",
+        "URL": "./Songs/maroon-5-girls-like-you.mp3"
+      },
+      {
+        "Title": "Right Now",
+        "Artist": "Nick Jonas",
+        "URL": "./Songs/Nick_Jonas_Robin_Schulz_Right_Now.mp3"
+      },
+      {
+        "Title": "See You Again",
+        "Artist": "Wiz Khalifa feat. Charlie Puth",
+        "URL": "./Songs/see-you-again-ft-charlie-puth.mp3"
+      },
+      {
+        "Title": "Senorita",
+        "Artist": "Camila Cabello and Shawn Mendes",
+        "URL": "./Songs/senorita (mp3cut.net) (1).mp3"
+      },
+      {
+        "Title": "Taki Taki",
+        "Artist": "DJ Snake",
+        "URL": "./Songs/taki-taki-mp3-free-download.mp3"
+      },
+      {
+        "Title": "Wonder",
+        "Artist": "Shawn Mendes",
+        "URL": "./Songs/Wonder - Shawn Mendes.mp3"
+      },
+      {
+        "Title": "Dusk Till Dawn",
+        "Artist": "Zayn Malik",
+        "URL": "./Songs/Dusk Till Dawn.mp3"
+      },
+      
+]
+
--- script.js
+++ script.js
@@ -1,166 +1,188 @@
-let rewind=document.querySelector(".rewind")
-let playpausebtn=document.querySelector(".play-pause")
-let forward=document.querySelector(".forward")
-let timer=document.querySelector(".timer")
-let volume=document.querySelector(".volume")
+window.onload = function () {
+  Particles.init({
+    selector: ".background",
+    maxParticles: 150,
+    color: "white",
+    sizeVariations: 3,
+  });
+};
 
-let startmin=document.querySelector(".minute")
-let startsec=document.querySelector(".second")
-let endmin=document.querySelector(".end-minute")
-let endsec=document.querySelector(".end-second")
+let rewind = document.querySelector(".rewind");
+let playpausebtn = document.querySelector(".play-pause");
+let forward = document.querySelector(".forward");
+let timer = document.querySelector(".timer");
+let volume = document.querySelector(".volume");
 
-let volumeBtn = document.querySelector(".volume")
-let volumeSliderContainer=document.querySelector(".volumeSliderContainer")
-let volumeSlider=document.querySelector("#volumeSlider")
-// let volslidervisible=false;
+let startmin = document.querySelector(".minute");
+let startsec = document.querySelector(".second");
+let endmin = document.querySelector(".end-minute");
+let endsec = document.querySelector(".end-second");
 
-volumeBtn.addEventListener("mouseover",function(){
-    volumeSliderContainer.style.visibility = "visible";
-})
+let volumeBtn = document.querySelector(".volume");
+let volumeSliderContainer = document.querySelector(".volumeSliderContainer");
+let volumeSlider = document.getElementById("volumeSlider");
 
-volumeBtn.addEventListener("mouseleave",function(){
-    volumeSliderContainer.style.visibility = "hidden";
-})
+let trackindex = 0;
+let current;
 
-volumeSliderContainer.addEventListener("click",function(e){
-    console.log(e)
-    // source.volume=
-})
+forward.addEventListener("click", function () {
+  songplaying = false;
+  playpausebtn.innerText = "play_circle_filled";
+  player.load();
+  setTimeout(() => {
+    playpausebtn.click();
+  }, 300);
+  document.querySelector(".active").classList.remove("active");
+  nextTrack();
+});
 
-let songplaying=false;
-
-
-let source=document.querySelector("#player")
-
-playpausebtn.addEventListener("click",function(){
-    if(!songplaying){
-        songplaying=true;
-        playpausebtn.innerText="pause_circle_outline"
-        source.play()
-        console.log("play")
-
-    }
-    else{
-        songplaying=false;
-        playpausebtn.innerText="play_circle_filled"
-        source.pause()
-        console.log("pause")
-
-    }
-
-})
-
-function createSongList(){
-
-    let list=document.createElement("div")
-    list.classList.add("song-list");
-    // let heading=document.createElement("h2") 
-    // heading.appendChild(document.createTextNode("Songs"))
-    // list.appendChild(heading)
-  
-    for(let i=0;i<allSongs.length;i++){
-
-        let songinfo=document.createElement("div") 
-        songinfo.classList.add("songinfo") 
-        songinfo.classList.add(`${i}`) 
-
-        let title=document.createElement("div")
-        // title.classList.add("title")
-        title.appendChild(document.createTextNode(allSongs[i]["Title"]))
-        songinfo.appendChild(title)
-
-        let artist=document.createElement("div")
-        // artist.classList.add(artist)
-        artist.appendChild(document.createTextNode(allSongs[i]["Artist"]))
-        songinfo.appendChild(artist)
-
-        list.appendChild(songinfo)
-    }
-    return list
+rewind.addEventListener("click", function () {
+  songplaying = false;
+  playpausebtn.innerText = "play_circle_filled";
+  player.load();
+  setTimeout(() => {
+    playpausebtn.click();
+  }, 300);
+  document.querySelector(".active").classList.remove("active");
+  prevTrack();
+});
+function nextTrack() {
+  if (trackindex < Songs.length - 1) {
+    trackindex += 1;
+  } else trackindex = 0;
+  source.src = Songs[trackindex]["URL"];
+  var nexttarget = document.getElementsByClassName(trackindex);
+  nexttarget[0].classList.add("active");
 }
-document.querySelector(".right-container").appendChild(createSongList())
-// createSongList()
 
-document.querySelectorAll('.songinfo').forEach(item => {
-    item.addEventListener('click',function(e){
-        // let source=document.querySelector("#player")
-        let i=e.currentTarget.classList[1];  //getting id of current song to get its url
-        let url=source.src=allSongs[i]["URL"]
-        console.log(source)
-        playmusic(url);
-        // let player=document.querySelector("#player")
-        // console.log(player)
-        // player.load()
-        // source.play()
+function prevTrack() {
+  console.log("prevtrack " + typeof trackindex);
+  if (trackindex > 0) trackindex -= 1;
+  else trackindex = Songs.length - 1;
+  source.src = Songs[trackindex]["URL"];
+  console.log(trackindex);
+  var nexttarget = document.getElementsByClassName(trackindex);
+  nexttarget[0].classList.add("active");
+}
 
-    } )
-})
+
+volumeSlider.addEventListener("change", function (e) {
+  const volume = e.target.value;
+  volumeSlider.value = volume;
+  player.volume = volume;
+});
+
+let songplaying = false;
+
+let source = document.querySelector("#source");
+let player = document.querySelector("#player");
+
+playpausebtn.addEventListener("click", function () {
+  if (!songplaying) {
+    songplaying = true;
+    playpausebtn.innerText = "pause_circle_outline";
+    player.play();
+    console.log("play in playpausebtn");
+  } else {
+    songplaying = false;
+    playpausebtn.innerText = "play_circle_filled";
+    player.pause();
+    console.log("pause in playpausebtn");
+  }
+});
+
+function createSongList() {
+  let list = document.createElement("div");
+  list.classList.add("song-list");
+  for (let i = 0; i < Songs.length; i++) {
+    let songinfo = document.createElement("div");
+    songinfo.classList.add("songinfo");
+    songinfo.classList.add(`${i}`);
+    if (i == 0) songinfo.classList.add("active");
+
+    let title = document.createElement("div");
+    title.appendChild(document.createTextNode(Songs[i]["Title"]));
+    songinfo.appendChild(title);
+
+    let artist = document.createElement("div");
+    artist.appendChild(document.createTextNode(Songs[i]["Artist"]));
+    songinfo.appendChild(artist);
+
+    list.appendChild(songinfo);
+  }
+  return list;
+}
+document.querySelector(".right-container").appendChild(createSongList());
+
+
+function addactive(e) {
+  current = document.querySelector(".active");
+  console.log(current);
+  if (current) current.classList.remove("active");
+  e.currentTarget.classList.add("active");
+}
+
+document.querySelectorAll(".songinfo").forEach((item) => {
+  item.addEventListener("click", function (e) {
+    addactive(e);
+    let i = e.currentTarget.classList[1]; //getting id of current song to get its url
+    trackindex = parseInt(i);
+    source.src = Songs[i]["URL"];
+    songplaying = false;
+    playpausebtn.innerText = "play_circle_filled";
+    player.load();
+    setTimeout(() => {
+      playpausebtn.click();
+    }, 100);
+    updateProgressBar(e);
+  });
+});
 
 let timerInterval;
 let second = 0;
 let minute = 0;
 
+let progbar = document.querySelector(".progress-bar-container");
 
-let progbar=document.querySelector(".progress-bar-container")
+progbar.addEventListener("click", function (e) {
+  player.currentTime = (e.offsetX / progbar.offsetWidth) * player.duration;
+});
 
-progbar.addEventListener("click",function(e){
-    console.log("a "+(e.offsetX/progbar.offsetWidth)*source.duration)
-    source.currentTime=((e.offsetX/progbar.offsetWidth)*source.duration);
-})
+function updateProgressBar(e) {
+  //updating progress bar
 
+  let { currentTime, duration } = e.srcElement;
+  timer.style.width = Math.floor((currentTime / duration) * 100) + "%";
 
-function updateProgressBar(e){
+  // Calculate display for duration
+  let durationMinutes = Math.floor(duration / 60);
+  let durationSeconds = Math.floor(duration % 60);
+  if (durationSeconds < 10) {
+    durationSeconds = `0${durationSeconds}`;
+  }
 
-    //updating progress bar
-    let {currentTime,duration} = e.srcElement;
-    timer.style.width=Math.floor((currentTime/duration)* 100) + "%";
-
-    // Calculate display for duration
-    let durationMinutes = Math.floor(duration / 60);
-    let durationSeconds = Math.floor(duration % 60);
-    if (durationSeconds < 10) {
-        durationSeconds = `0${durationSeconds}`;
+  // Delay switching duration Element to avoid NaN
+  if (durationSeconds) {
+    if (durationMinutes < 10) {
+      durationMinutes = `0${durationMinutes}`;
     }
+    endmin.innerText = durationMinutes;
+    endsec.innerText = durationSeconds;
+  }
 
-    // Delay switching duration Element to avoid NaN
-    if (durationSeconds) {
-        // durationEl.textContent = `${durationMinutes}:${durationSeconds}`;    
-        if (durationMinutes < 10) {
-            durationMinutes=`0${durationMinutes}`;
-        }
-        endmin.innerText=durationMinutes;
-        endsec.innerText=durationSeconds
-    }
+  // Calculate display for currentTime
+  let currentMinutes = Math.floor(currentTime / 60);
+  let currentSeconds = Math.floor(currentTime % 60);
 
-    // Calculate display for currentTime
-    let currentMinutes = Math.floor(currentTime / 60);
-    let currentSeconds = Math.floor(currentTime % 60);
-        
-    if (currentMinutes < 10) {
-        currentMinutes = `0${currentMinutes}`;
-    }
-    if (currentSeconds < 10) {
-        currentSeconds = `0${currentSeconds}`;
-    }
-    
-    startmin.innerText=currentMinutes;
-    startsec.innerText=currentSeconds
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  if (currentSeconds < 10) {
+    currentSeconds = `0${currentSeconds}`;
+  }
 
+  startmin.innerText = currentMinutes;
+  startsec.innerText = currentSeconds;
 }
 
-source.addEventListener("timeupdate",updateProgressBar)
-
-
-// async function playmusic(url){
-//     await tab.goto(url)
-//     await tab.click(".s_l.artworkload ")
-// }
-
-
-
-
-
-
-
-
-
+player.addEventListener("timeupdate", updateProgressBar);
